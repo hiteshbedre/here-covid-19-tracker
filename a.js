d3056837fedf6a2f550a80b7f533f37d5879ba05
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2021-10-09 13:45:52 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,235 @@
-package com.arshilgenius.kisan.agriculture.sync;
+     // Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for the NumericExpressionInput interaction.
+ *
+ * IMPORTANT NOTE: The naming convention for customization args that are passed
+ * into the directive is: the name of the parameter, followed by 'With',
+ * followed by the name of the arg.
+ */
+
+require(
+  'interactions/NumericExpressionInput/directives/' +
+  'numeric-expression-input-rules.service.ts');
+require('interactions/interaction-attributes-extractor.service.ts');
+require(
+  'pages/exploration-player-page/services/current-interaction.service.ts');
+require('services/contextual/device-info.service.ts');
+require('services/guppy-configuration.service.ts');
+require('services/guppy-initialization.service.ts');
+require('services/math-interactions.service.ts');
+
+angular.module('oppia').component('oppiaInteractiveNumericExpressionInput', {
+  template: require('./numeric-expression-input-interaction.component.html'),
+  bindings: {
+    savedSolution: '<'
+  },
+  controller: [
+    '$attrs', '$scope', 'CurrentInteractionService', 'DeviceInfoService',
+    'GuppyConfigurationService', 'GuppyInitializationService',
+    'InteractionAttributesExtractorService', 'MathInteractionsService',
+    'NumericExpressionInputRulesService',
+    function(
+        $attrs, $scope, CurrentInteractionService, DeviceInfoService,
+        GuppyConfigurationService, GuppyInitializationService,
+        InteractionAttributesExtractorService, MathInteractionsService,
+        NumericExpressionInputRulesService) {
+      const ctrl = this;
+      ctrl.value = '';
+      ctrl.hasBeenTouched = false;
+      ctrl.warningText = '';
+
+      ctrl.isCurrentAnswerValid = function() {
+        let activeGuppyObject = (
+          GuppyInitializationService.findActiveGuppyObject());
+        if (ctrl.hasBeenTouched && activeGuppyObject === undefined) {
+          // Replacing abs symbol, '|x|', with text, 'abs(x)' since the symbol
+          // is not compatible with nerdamer or with the backend validations.
+          ctrl.value = MathInteractionsService.replaceAbsSymbolWithText(
+            ctrl.value);
+          let answerIsValid = MathInteractionsService.validateNumericExpression(
+            ctrl.value);
+          if (answerIsValid) {
+            // Explicitly inserting '*' signs wherever necessary.
+            ctrl.value = MathInteractionsService.insertMultiplicationSigns(
+              ctrl.value);
+          }
+          ctrl.warningText = MathInteractionsService.getWarningText();
+          return answerIsValid;
+        }
+        ctrl.warningText = '';
+        return true;
+      };
+
+      ctrl.submitAnswer = function() {
+        if (!ctrl.isCurrentAnswerValid()) {
+          return;
+        }
+        CurrentInteractionService.onSubmit(
+          ctrl.value, NumericExpressionInputRulesService);
+      };
+
+      ctrl.showOSK = function() {
+        GuppyInitializationService.setShowOSK(true);
+        GuppyInitializationService.interactionType = 'NumericExpressionInput';
+      };
+
+      ctrl.$onInit = function() {
+        ctrl.hasBeenTouched = false;
+        GuppyConfigurationService.init();
+        const { useFractionForDivision, placeholder } = (
+          InteractionAttributesExtractorService.getValuesFromAttributes(
+            'NumericExpressionInput', $attrs));
+        // This represents a list of special characters in LaTeX. These
+        // characters have a special meaning in LaTeX and thus need to be
+        // escaped.
+        const escapeCharacters = [
+          '&', '%', '$', '#', '_', '{', '}', '~', '^', '\\'];
+        for (var i = 0; i < placeholder.unicode.length; i++) {
+          if (escapeCharacters.includes(placeholder.unicode[i])) {
+            let newPlaceholder = `\\verb|${placeholder.unicode}|`;
+            placeholder.unicode = newPlaceholder;
+            break;
+          }
+        }
+        GuppyConfigurationService.changeDivSymbol(useFractionForDivision);
+        GuppyInitializationService.init(
+          'guppy-div-learner',
+          placeholder.unicode,
+          ctrl.savedSolution !== undefined ?
+          ctrl.savedSolution : ''
+        );
+        let eventType = (
+          DeviceInfoService.isMobileUserAgent() &&
+          DeviceInfoService.hasTouchEvents()) ? 'focus' : 'change';
+        // We need the 'focus' event while using the on screen keyboard (only
+        // for touch-based devices) to capture input from user and the 'change'
+        // event while using the normal keyboard.
+        Guppy.event(eventType, () => {
+          var activeGuppyObject = (
+            GuppyInitializationService.findActiveGuppyObject());
+          if (activeGuppyObject !== undefined) {
+            ctrl.hasBeenTouched = true;
+            ctrl.value = activeGuppyObject.guppyInstance.asciimath();
+            if (eventType === 'change') {
+              // Need to manually trigger the digest cycle to make any
+              // 'watchers' aware of changes in answer.
+              $scope.$apply();
+            }
+          }
+        });
+
+        CurrentInteractionService.registerCurrentInteraction(
+          ctrl.submitAnswer, ctrl.isCurrentAnswerValid);
+      };
+    }
+  ]
+});
+'store_success' => 'The work experience was successfully added.',
+                    'update_success' => 'The work experience details were successfully updated.',
+                    'delete_success' => 'The work experience was successfully removed.',
+                ],
+                'education' => [
+                    'main' => 'Education',
+                    'add_new' => 'Add education',
+                    'edit_details' => 'Edit education details',
+                    'type' => 'Type',
+                    'institution' => 'Institution',
+                    'major' => 'Major',
+                    'year' => 'Year',
+                    'grade' => 'Grade',
+                    'start_date' => 'Start date',
+                    'end_date' => 'End date',    
+                    'store_success' => 'The education details were successfully added.',
+                    'update_success' => 'The education details were successfully updated.',
+                    'delete_success' => 'The education details were successfully removed.',
+                    'education_types' => [
+                        'bachelor' => "Bachelor's degree",
+                        'master' => "Master's degree",
+                        'academy' => "Academy certificate",
+                        'undergrad' => "Undergraduate"
+                    ]
+                ],
+                'languages' => [
+                    'main' => 'Languages',
+                    'add_new' => 'Add language',
+                    'edit_details' => 'Edit language',
+                    'language' => 'Language',
+                    'level' => 'Level',
+                    'skill' => 'Skill',
+                    'store_success' => 'The language was successfully added to the profile.',
+                    'update_success' => 'The language details were successfully updated.',
+                    'delete_success' => 'The language was successfully removed.',
+                    'skills' => [
+                        'write' => 'Written',
+                        'speak' => 'Spoken',
+                        'both' => 'Written and Spoken',
+                    ],
+                    'levels' => [
+                        'beginner' => 'Beginner',
+                        'intermediate' => 'Intermediate',
+                        'fluent' => 'Fluent',
+                        'native' => 'Native',
+                    ]
+                ]
+            ],
+            'salaries' => [
+                'main' => 'Salaries',
+                'add_new' => 'Add salary record',
+                'edit_details' => 'Edit salary record',
+                'salary_history' => 'Salary history',
+                'gross_total' => 'Gross total',
+                'nett_total' => 'Nett total',
+                'payment_date' => 'Payment date',
+                'attachment' => 'Attachment',
+                'attachmed_file' => 'Attached file',
+                'salary_setup' => 'Set Current Salary',
+                'salary_report' => 'Salary Report',
+                'store_success' => 'The salary record was successfully added.',
+                'update_success' => 'The salary details were successfully updated.',
+                'delete_success' => 'The salary record was successfully removed.',
+                'config' => [
+                    'amount' => 'Amount',
+                    'type' => 'Type',
+                    'currency' => 'Currency',
+                    'type_full' => 'Flat',
+                    'type_hour' => 'Hourly',
+                    'bank_account' => 'Bank Account',
+                    'id_number' => 'ID Number',
+                ],
+            ]
+        ],
+        'candidates' => [
+            'main' => 'Candidates',
+            'add_new' => 'Add candidate',
+            'edit_details' => 'Edit candidate details',
+            'store_success' => 'The candidate was successfully added.',
+            'update_success' => 'The candidate details were successfully updated.',
+            'delete_success' => 'The candidate was successfully removed.',
+            'additional' => 'Additional details',
+            'mark_featured_title' => 'Mark as important to have the candidate featured on the dashboard',
+            'preferences' => [
+                'main' => 'Preferences and expectations',
+                'add_new' => 'Add preferences',
+                'edit_details' => 'Edit preferences',
+                'salary' => 'Minimal salary',
+                'contract_type' => 'Contract type',
+                'comments' => 'Comments',
+                'location' => [
+                    'main' => 'Location',
+                    'remote' => package com.arshilgenius.kisan.agriculture.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
